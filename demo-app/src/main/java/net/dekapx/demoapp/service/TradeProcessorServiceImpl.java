7750f764aae67dc122e49421e095f31410586d36
dekapx <xkapil@gmail.com>
2022-02-21 12:39:44 +0000

    code cleanup

--- demo-app/src/main/java/net/dekapx/demoapp/service/TradeProcessorServiceImpl.java
+++ demo-app/src/main/java/net/dekapx/demoapp/service/TradeProcessorServiceImpl.java
@@ -3,6 +3,8 @@ package net.dekapx.demoapp.service;
 import lombok.extern.slf4j.Slf4j;
 import net.dekapx.demoapp.domain.TradeHistory;
 import net.dekapx.demoapp.domain.TradeTransaction;
+import net.dekapx.demoapp.handler.TradeHandlerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Function;
@@ -10,6 +12,14 @@ import java.util.function.Function;
 @Slf4j
 @Service
 public class TradeProcessorServiceImpl implements TradeProcessorService {
+    private TradeHandlerFactory factory;
+
+    @Autowired
+    public TradeProcessorServiceImpl(final TradeHandlerFactory factory) {
+        this.factory = factory;
+    }
+
+
     @Override
     public void processTransactions(final TradeTransaction tradeTransaction) {
 
